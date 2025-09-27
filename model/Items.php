b65385b0fd19c80a54397d8b2f9c9aed2e38e3ec
Mario Florez <mario.florez249@gmail.com>
2025-09-27 15:05:14 -0500

    Se implementa la lógica de crear, actualizar y desactivar/eliminar del modelo item

--- model/Items.php
+++ model/Items.php
@@ -3,7 +3,7 @@
         public function get_item(){
             $connect= parent::connection();
             parent::set_names();
-            $sql="SELECT * FROM tm_item";
+            $sql="SELECT * FROM tm_item WHERE status=1";
             $sql=$connect->prepare($sql);
             $sql->execute();
             return $resultado=$sql->fetchAll();
@@ -15,6 +15,64 @@
             $sql="SELECT * FROM tm_item WHERE item_id = ?";
             $sql=$connect->prepare($sql);
             $sql->bindValue(1,$item_id)
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function delete_item($item_id){
+            $connect= parent::connection();
+            parent::set_names();
+            $sql="UPDATE tm_item
+                SET 
+                    status=0,
+                    delete_date=now()
+                WHERE
+                    item_id = ?
+                ";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1,$item_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function insert_item($item_name){
+            $connect= parent::connection();
+            parent::set_names();
+            $sql="INSERT INTO tm_item (
+                    item_id
+                    item_name,
+                    creation_date,
+                    update_date,
+                    delete_date,
+                    status
+                )
+                VALUES (
+                    NULL,
+                    ?,
+                    now(),
+                    NULL,
+                    NULL,
+                    1
+                )";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1,$item_name);
+            $sql->execute();
+            return $resultado=$sql->fetchAll();
+        }
+
+        public function update_item($item_id, $item_name){
+            $connect= parent::connection();
+            parent::set_names();
+            $sql="UPDATE tm_item
+                SET 
+                    name=?,
+                    update_date=now()
+                WHERE
+                    item_id = ?
+                ";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1,$item_name);
+            $sql->bindValue(2,$item_id);
             $sql->execute();
             return $resultado=$sql->fetchAll();
         }
