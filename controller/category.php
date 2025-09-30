8dc69e20f25974e84121c0e943fe3db6b66a3d29
Mario Florez <mario.florez249@gmail.com>
2025-09-29 23:12:40 -0500

    Se desarrolla una API Básica para la entidad Categoría, TODO: refinar hasta lograr una API RESTFULL

--- controller/category.php
+++ controller/category.php
@@ -1,9 +1,37 @@
 <?php
+    header('Content-Type: application/json');
+
     require_once("../config/connection.php");
     require_once("../model/Category.php");
     $category = new Category();
+    $body=json_decode(file_get_contents("php://input"), true);
 
     switch($_GET["op"]){
+
+        case "GetAll":
+            $data=$category->get_categories();
+            echo json_encode($data);
+            break;
+
+        case "GetCategory":
+            $data=$category->get_category_by_id($_GET["id"]);
+            echo json_encode($data);
+            break;
+
+        case "Create":
+            $data=$category->insert_category($body["name"]);
+            echo json_encode("Insertado correctamente");
+            break;
+        
+        case "Update":
+            $data=$category->update_category($body["id"], $body["name"]);
+            echo json_encode("Actualizado correctamente");
+            break;
+
+        case "Delete":
+            $data=$category->delete_category($_GET["id"]);
+            echo json_encode("Desactivado correctamente");
+            break;
 
         case "combo":
             $data=$category->get_categories();
--- model/Category.php
+++ model/Category.php
@@ -6,7 +6,54 @@
             $sql="SELECT * FROM tm_category WHERE category_status=1";
             $sql=$connect->prepare($sql);
             $sql->execute();
-            return $resultado=$sql->fetchAll();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function get_category_by_id($id){
+            $connect= parent::connection();
+            parent::set_names();
+            $sql="SELECT * FROM tm_category WHERE category_id=?";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1, $id);
+            $sql->execute();
+            return $result=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_category($name){
+            $connect=parent::connection();
+            parent::set_names();
+            $sql="INSERT INTO tm_category
+                (category_id, category_name, category_status)
+                VALUES ( NULL,?,1)";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1, $name);
+            $sql->execute();
+            return $result=$sql->fetchAll();    
+        }
+
+        public function update_category($id, $name){
+            $connect=parent::connection();
+            parent::set_names();
+            $sql="UPDATE tm_category
+                SET  category_name=?
+                WHERE category_id=?";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1, $name);
+            $sql->bindValue(2, $id);
+            $sql->execute();
+            return $result=$sql->fetchAll();    
+        }
+
+        public function delete_category($id){
+            $connect=parent::connection();
+            parent::set_names();
+            $sql="UPDATE tm_category
+                SET  category_status=0
+                WHERE category_id=?";
+            $sql=$connect->prepare($sql);
+            $sql->bindValue(1, $id);
+            $sql->execute();
+            return $result=$sql->fetchAll();    
         }
 
     }
